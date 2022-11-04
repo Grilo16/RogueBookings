0b56c56da2ac694e3b2411ec2b74abdf54cc4a5c
Mhairi MacDonald <mhairilouyoga@gmail.com>
2022-11-04 07:01:41 +0000

    added calendar

--- RogueBookingsClient/src/components/UserDashboard/ContentContainer/Calendar/Calendar.jsx
+++ RogueBookingsClient/src/components/UserDashboard/ContentContainer/Calendar/Calendar.jsx
@@ -1,9 +1,16 @@
-function Calendar(){
-    return(
-        <>
-            <p>Calendar</p>
-        </>
-    )
+import React from "react";
+import ReactCalendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useState } from "react";
+
+const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const onDateChanged = (date) => {
+    setSelectedDate(date);
+  };
+
+  return <ReactCalendar onChange={onDateChanged} />;
 };
 
 export default Calendar;
